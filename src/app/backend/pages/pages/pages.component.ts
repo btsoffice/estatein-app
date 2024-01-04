f1280a04e17e1c2bf7e68f8f62b01f97ed6dc93a
programmer1319 <muneeb.workstation@gmail.com>
2024-01-05 03:06:50 +0500

    Dashboard Started

--- src/app/backend/pages/pages/pages.component.ts
+++ src/app/backend/pages/pages/pages.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { PagesService } from '../../services/pages.service';
 import { PageData } from '../../interfaces/pages';
-
+import { TableModule} from 'primeng/table';
+import { AvatarModule } from 'primeng/avatar';
 @Component({
   selector: 'app-pages',
   standalone: true,
-  imports: [],
+  imports: [TableModule, AvatarModule],
   templateUrl: './pages.component.html',
   styleUrl: './pages.component.scss'
 })
 export class PagesComponent implements OnInit {
   baseUrl = 'http://localhost:3333';
   pageData: PageData[] = [];
+  first: number = 0;
+  rows: number = 1;
   constructor(private pageService: PagesService) { }
   ngOnInit(): void {
     this.pageService.getPages().subscribe({
@@ -19,6 +22,9 @@ export class PagesComponent implements OnInit {
       error: (error) => console.error(error)
     });
   }
-  
+  pageChange(event: { first: number; rows: number; }){
+    this.first = event.first;
+    this.rows = event.rows
+  }
 
 }
--- src/app/backend/shared/sidebar/sidebar.component.ts
+++ src/app/backend/shared/sidebar/sidebar.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { PanelMenuModule } from 'primeng/panelmenu';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [PanelMenuModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
-export class SidebarComponent {
-
+export class SidebarComponent implements OnInit{
+  items!: MenuItem[];
+  ngOnInit(){
+    this.items = [
+      {
+        label: 'Navigation',
+        items: [
+          {
+            label: 'Pages',
+            routerLink: '/admin/pages',
+            items: [
+              {
+                label: 'Home',
+                icon: 'pi pi-fw pi-plus',
+                routerLink: '/admin/home'
+              },
+              {
+                label: 'About',
+                icon: 'pi pi-fw pi-plus',
+                routerLink: '/admin/about'
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
 }
